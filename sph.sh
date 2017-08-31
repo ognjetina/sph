f921a78ba0d89d9bf1bbb1bce9570d710ac90036
Ognjen Lazic <ognjen.lazic@codecentric.de>
2017-08-31 16:35:01 +0200

    Add new device functions

--- sph.sh
+++ sph.sh
@@ -30,6 +30,29 @@ sph ()
 alias shakedevice='adb shell input keyevent 82'
 alias reversedevice='adb reverse tcp:8081 tcp:8081'
 
+function device {
+echo '-->  Type device -s To shake the device'
+echo '-->  Type device -r To reverse device TCP 8081'
+echo '---> Type device -c <app-name> To clear dev app data'
+if test "$1" == "-s"
+then
+        adb shell input keyevent 82
+        echo '-->  Shake complete'
+else
+        if test "$1" == "-r"
+        then
+        adb reverse tcp:8081 tcp:8081
+        echo '-->  Reverse complete'
+        else
+            if test "$1" == "-c"
+            then
+            adb shell pm clear $2
+            echo '-->  Data wiped'
+            fi
+        fi
+fi
+}
+
 # start andorid virtual device
 function va {
 echo '-->  Type va -l (to list devices)'
