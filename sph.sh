5bd0d6c36884026ce3909d1cae482aee6dab7d5f
Ognjen Lazic <ognjen.lazic@codecentric.de>
2017-10-10 14:33:17 +0200

    Add screenshot command

--- sph.sh
+++ sph.sh
@@ -73,3 +73,11 @@ else
         fi
 fi
 }
+
+#pull screenshot from phone
+function ssandroid {
+    adb shell screencap -p /sdcard/screen.png
+    adb pull /sdcard/screen.png
+    adb shell rm /sdcard/screen.png
+    mv screen.png ~/Desktop
+}
